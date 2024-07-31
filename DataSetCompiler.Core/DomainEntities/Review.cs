4432a2056c5ccecc36c319f9ac431cc9972d78e6
MeseryGG <meserygamer@gmail.com>
2024-07-31 17:45:33 +0300

    Fix: Captcha issue

--- DataSetCompiler.Core/DomainEntities/Review.cs
+++ DataSetCompiler.Core/DomainEntities/Review.cs
@@ -7,6 +7,9 @@ public class Review
 {
     [JsonPropertyName("title")] 
     public string ReviewTitle { get; set; } = null!;
+
+    [JsonPropertyName("review_opinion")]
+    public string ReviewOpinion { get; set; } = null!;
     
     [JsonPropertyName("text")] 
     public string ReviewText { get; set; } = null!;
