8ddbd370296a72bcdab04fc4cb69868f0bc81993
MeseryGamer <meserygamer@outlook.com>
2024-08-03 00:49:18 +0300

    Move: settings into file

---
+++ DataSetCompiler.API/AppSettings/KinopoiskParserSettings.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace DataSetCompiler.API.AppSettings;
+
+public class KinopoiskParserSettings
+{
+    [JsonPropertyName("cookies")]
+    public string? Cookies { get; set; }
+    
+    [JsonPropertyName("films_urls")]
+    public ICollection<string> FilmsUrls { get; set; } = null!;
+}
